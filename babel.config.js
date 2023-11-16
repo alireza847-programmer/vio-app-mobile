38a70f325290a22332b3e0552c218b3950653e76
alireza <alireza@alirezas-MacBook-Pro.local>
2023-11-16 17:20:33 +0330

    ADD aliases to the codebase

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          components: './src/components',
+          themes: './src/themes',
+          utils: './src/utils',
+          routes: './src/routes',
+          types: './src/types',
+          pages: './src/pages',
+          assets: './src/assets',
+          layouts: './src/layouts',
+          translation: './src/translation',
+          hooks: './src/hooks',
+        },
+      },
+    ],
+  ],
 };
