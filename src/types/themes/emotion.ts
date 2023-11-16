b5870f32ed4e60d4af562ebbbe0b1df1b03a38f6
alireza <alireza@alirezas-MacBook-Pro.local>
2023-11-16 17:49:35 +0330

    ADD theme file configuration in themes directory

---
+++ src/types/themes/emotion.ts
@@ -0,0 +1,38 @@
+interface FontProps {
+  fontFamily: string;
+  fontSize: number;
+}
+
+export interface EmotionThemeType {
+  colors: {
+    backgroundColor: string;
+    textColor: {
+      primary: string;
+      placeHolder: string;
+      button: {
+        primary: string;
+        secondary: string;
+        error: string;
+      };
+    };
+    button: {
+      primary: string;
+      secondary: string;
+    };
+    stroke: {
+      button: string;
+      divider: string;
+      input: string;
+    };
+  };
+  fonts: {
+    bold21: FontProps;
+    medium15: FontProps;
+    semiBold16: FontProps;
+    semiBold18: FontProps;
+    regular16: FontProps;
+  };
+  verticalRem: number;
+  HorizontalRem: number;
+  radius: number;
+}
