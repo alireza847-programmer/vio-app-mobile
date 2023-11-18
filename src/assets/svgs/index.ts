ef6c99efa83f4ef46b83787a044d30122d536535
a.sheykholeslami <a.sheykholeslami@kian.digital>
2023-11-18 14:11:03 +0330

    ADD chevron.svg file

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,4 @@
 import PencilEditButton from 'assets/svgs/pencilEditButton.svg';
+import ChevronSvg from 'assets/svgs/chevron.svg';
 
-export {PencilEditButton};
+export {PencilEditButton, ChevronSvg};
