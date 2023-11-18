c30ec8240d3145f98b7e40c50eef9c8c8a83ed63
a.sheykholeslami <a.sheykholeslami@kian.digital>
2023-11-18 16:09:26 +0330

    ADD button component Doc

--- src/components/uiElements/button/style.ts
+++ src/components/uiElements/button/style.ts
@@ -5,12 +5,15 @@ const Button = styled.TouchableOpacity<ButtonProps>(
   ({theme, mode = 'primary', styled = 'FULL_WIDTH'}) => ({
     flexDirection: 'row',
     justifyContent: 'center',
-    paddingVertical: theme.verticalRem * 4,
+    alignItems: 'center',
     borderRadius: theme.radius * 2,
     borderColor: theme.colors.stroke.button,
     ...(styled === 'FULL_WIDTH' && {width: '100%'}),
     ...(styled === 'CONTENT_SIZE' && {
       paddingHorizontal: theme.HorizontalRem * 4,
+    }),
+    ...(styled !== 'TEXT' && {
+      paddingVertical: theme.verticalRem * 4,
     }),
     ...(styled !== 'TEXT' && {backgroundColor: theme.colors.button[mode]}),
     ...(mode === 'secondary' && styled !== 'TEXT' && {borderWidth: 1}),
