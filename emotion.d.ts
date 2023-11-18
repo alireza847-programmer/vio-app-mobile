f49a0cb5fb31623ed2c818458ad73e0644f57475
a.sheykholeslami <a.sheykholeslami@kian.digital>
2023-11-18 12:05:59 +0330

    FIX emotion theme type in declaration madule emotion.d.ts

--- emotion.d.ts
+++ emotion.d.ts
@@ -1,42 +1,7 @@
 import '@emotion/react';
+import {EmotionThemeType} from 'types/themes/emotion';
 
-interface FontProps {
-  fontFamily: string;
-  fontSize: number;
-}
-
+// Augment the module
 declare module '@emotion/react' {
-  export interface Theme {
-    colors: {
-      backgroundColor: string;
-      textColor: {
-        primary: string;
-        placeHolder: string;
-        button: {
-          primary: string;
-          secondary: string;
-          error: string;
-        };
-      };
-      button: {
-        primary: string;
-        secondary: string;
-      };
-      stroke: {
-        button: string;
-        divider: string;
-        input: string;
-      };
-    };
-    fonts: {
-      bold21: FontProps;
-      medium15: FontProps;
-      semiBold16: FontProps;
-      semiBold18: FontProps;
-      regular16: FontProps;
-    };
-    verticalRem: number;
-    HorizontalRem: number;
-    radius: number;
-  }
+  export interface Theme extends EmotionThemeType {}
 }
