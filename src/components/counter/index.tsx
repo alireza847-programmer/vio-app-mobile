0c15b73f55fd150c9003b08bb2fbb89543054211
a.sheykholeslami <a.sheykholeslami@kian.digital>
2023-11-19 16:12:32 +0330

    ADD integration test for VCounter component

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -1,7 +1,7 @@
 import VButton from 'components/uiElements/button';
 import VRow from 'components/uiElements/row';
 import VText from 'components/uiElements/text';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {VCounterProps} from 'types/components/counter';
 import {counterTextStyle} from './style';
 import {MinusSvg, PlusSvg} from 'assets/svgs';
@@ -15,23 +15,32 @@ const VCounter = (props: VCounterProps) => {
     } else {
       setCount(value => value + 1);
     }
-    onChange(count);
   };
+  useEffect(() => {
+    onChange(count);
+  }, [count]);
   return (
-    <VRow minWidth={'50%'} justifyContent="space-between" fullWidth={false}>
+    <VRow
+      testID="v-counter"
+      minWidth={'50%'}
+      justifyContent="space-between"
+      fullWidth={false}>
       <VButton
+        testID="decrement-button"
         mode="secondary"
         styled="CONTENT_SIZE"
         icon={fill => <MinusSvg fill={fill} />}
         onPress={() => onAction('-')}
       />
       <VText
+        testID="count-text"
         marginTopRatio={0}
         style={counterTextStyle}
         typography="semiBold16">
         {count}
       </VText>
       <VButton
+        testID="increment-button"
         mode="secondary"
         styled="CONTENT_SIZE"
         icon={fill => <PlusSvg fill={fill} />}
