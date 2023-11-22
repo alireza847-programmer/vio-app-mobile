abaee10b2557873b0ae3bd8e9beffade83cd7ffd
a.sheykholeslami <a.sheykholeslami@kian.digital>
2023-11-22 11:36:42 +0330

    UPDATE VButton doc and test

--- src/components/uiElements/button/button.test.tsx
+++ src/components/uiElements/button/button.test.tsx
@@ -1,39 +1,49 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
 import VButton from './index';
-import {it, describe, expect, jest} from '@jest/globals';
+import {it, describe, expect} from '@jest/globals';
 import {VButtonProps} from 'types/components/uiElements/button';
 import {ThemeProvider} from '@emotion/react';
 import {theme} from 'themes/emotion';
 
-const Component = (props: VButtonProps) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <VButton {...props} />
-    </ThemeProvider>
-  );
-};
+// A wrapper component to provide the theme context to VButton
+const Component = (props: VButtonProps) => (
+  <ThemeProvider theme={theme}>
+    <VButton {...props} />
+  </ThemeProvider>
+);
+
 describe('VButton component', () => {
-  it('matches snapshot with default props', () => {
+  it('renders correctly with default props', () => {
+    // Arrange
     const {toJSON} = render(<Component onPress={() => {}} />);
+
+    // Assert
     expect(toJSON()).toMatchSnapshot();
   });
 
-  it('matches snapshot with custom title and mode', () => {
+  it('renders correctly with custom title and mode', () => {
+    // Arrange
     const {toJSON} = render(
       <Component onPress={() => {}} title="Click me" mode="secondary" />,
     );
+
+    // Assert
     expect(toJSON()).toMatchSnapshot();
   });
 
-  it('matches snapshot with custom styled prop', () => {
+  it('renders correctly with custom styled prop', () => {
+    // Arrange
     const {toJSON} = render(
       <Component title="Click Me" onPress={() => {}} styled="CONTENT_SIZE" />,
     );
+
+    // Assert
     expect(toJSON()).toMatchSnapshot();
   });
 
-  it('matches snapshot with custom icon and rightIcon', () => {
+  it('renders correctly with custom icon and rightIcon', () => {
+    // Arrange
     const MockIcon = () => <svg>Mock Icon</svg>;
     const {toJSON} = render(
       <Component
@@ -42,6 +52,8 @@ describe('VButton component', () => {
         rightIcon={() => <MockIcon />}
       />,
     );
+
+    // Assert
     expect(toJSON()).toMatchSnapshot();
   });
 });
